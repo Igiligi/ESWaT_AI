a140183525416757d50a9adb0bf44bdb6e40d6c8
Chibueze Igiligi <johnboscoigiligi3@gmail.com>
2026-06-05 20:54:29 +0100

    Fix MIME type issue with explicit asset naming

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Change this line
+  base: './',
   build: {
     outDir: 'dist',
+    assetsInlineLimit: 0,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   }
